9ed8f7bccdba6cc60c98e9c26c24084aec2463eb
Jtech123 <alt.sa-8rme46w@yopmail.com>
2017-03-14 15:32:09 +0100

    Fixed popups

---
+++ webpageversion_5/gokkerswebpage/accounts.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+    $connection = mysqli_connect("localhost", "root", "", "gokkers") or die("ERROR: Could not connect!" . mysqli_connect_error());
+
+    if (isset($_POST['username']) && !empty ($_POST['username']))
+    {
+        $username = $_POST['username'];
+
+    }
+    if (isset($_POST['password']) && !empty ($_POST['password']))
+    {
+        $password = $_POST['password'];
+
+    }
+    if (isset($_POST['email']) && !empty ($_POST['email']))
+    {
+        $email = $_POST['email'];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            echo "Email is valid.";
+        }
+        else
+        {
+            echo "Email is invalid.";
+        }
+    }
+            $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
+            if(mysqli_query($connection, $sql))
+            {
+                echo "You successfully registered an account!";
+                $message = "You successfully registered an account!";
+                $_SESSION['message'] = $message;
+            }
+            else
+            {
+                echo "An error has occurred while registering";
+                $_SESSION['message'] = "An error has occurred while registering";
+            }
+
+            header("Location: index.php");
+?>
---
+++ webpageversion_5/gokkerswebpage/login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+$connection = mysqli_connect("localhost", "root", "", "gokkers");
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$loginQuery = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+
+$sql = mysqli_query($connection, $loginQuery);
+
+$rows = mysqli_fetch_assoc($sql);
+
+if ($rows["username"] == null || $rows["password"] == null)
+{
+    $message = "Login incorrect";
+}
+else
+{
+    $message =  "You are successfully logged in!";
+    $_SESSION['username'] = $username;
+    $_SESSION['message'] = $message;
+}
+
+header("Location: index.php");
+?>
