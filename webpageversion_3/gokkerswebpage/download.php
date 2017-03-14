458b4ffe3533753d6d44fef208426cd07ab0a14d
Snow076 <mike.dalmaijer.362@outlook.com>
2017-03-14 13:19:32 +0100

    Added page v3

---
+++ webpageversion_3/gokkerswebpage/download.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Download</title>
+</head>
+<body>
+    <h1>BETA 1: <a href="download/beta_1.rar">DOWNLOAD EXE</a></h1>
+</body>
+</html>
